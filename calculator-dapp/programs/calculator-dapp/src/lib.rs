c6856e1d224e6e5af17f0e390291550458c6dc5d
Stanley Lau <stanley@metabrge.com>
2022-08-08 15:36:09 +0800

    edit calculator

--- calculator-dapp/programs/calculator-dapp/src/lib.rs
+++ calculator-dapp/programs/calculator-dapp/src/lib.rs
@@ -1,4 +1,6 @@
 use anchor_lang::prelude::*;
+// use anchor_lang::solana_program::entrypoint::ProgramResult;
+// use anchor_lang::AccountsExit;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -6,10 +8,34 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod calculator_dapp {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create(_ctx: Context<Create>, _init_message: String) -> Result<()> {
+        let calculator = &mut _ctx.accounts.calculator;
+        calculator.greeting = _init_message;
         Ok(())
     }
 }
 
+#[account]
+#[derive(Default)]
+pub struct Calculator {
+    pub greeting: String,
+    pub result: i64,
+    pub remainder: i64
+}
+
+// retrieve the list of accounts' data
+// derive macro
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Create<'info> {
+    // init: init calculator account
+    // payer: who pay the cost for creating account
+    // space: the amount of space allocated for the account
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(init, payer=user, space=264)]
+    pub calculator: Account<'info, Calculator>,
+    // required for using 'account'
+    pub system_program: Program<'info, System>
+}
+
+
--- calculator-dapp/tests/calculator-dapp.ts
+++ calculator-dapp/tests/calculator-dapp.ts
@@ -1,16 +1,32 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
+import { expect } from "chai";
 import { CalculatorDapp } from "../target/types/calculator_dapp";
 
 describe("calculator-dapp", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.local();
+  anchor.setProvider(provider);
 
+  const calculator = anchor.web3.Keypair.generate(); // get address
   const program = anchor.workspace.CalculatorDapp as Program<CalculatorDapp>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Creates a calculator", async () => {
+
+    await program.methods
+      .create("hi")
+      .accounts({
+        calculator: calculator.publicKey,
+        user: provider.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([calculator])
+      .rpc();
+
+    const account = await program.account.calculator.fetch(
+      calculator.publicKey
+    );
+    
+    expect(account.greeting === "hi");
   });
 });
