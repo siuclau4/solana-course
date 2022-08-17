61d1463df157bbf2edddeac0ebe82dd867527edc
Stanley Lau <stanley@metabrge.com>
2022-08-17 10:04:16 +0800

    add crowd funding program

---
+++ crowd-funding/programs/crowd-funding/src/lib.rs
@@ -0,0 +1,74 @@
+use anchor_lang::prelude::*;
+use anchor_lang::solana_program::entrypoint::ProgramResult;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod crowd_funding {
+    use super::*;
+
+    pub fn create(_ctx: Context<Create>, _name:String, description:String) -> Result<()> {
+        let campaign = &mut _ctx.accounts.campaign;
+        campaign.name = _name;
+        campaign.description = description;
+        campaign.account_donated = 0;
+        campaign.admin = *_ctx.accounts.user.key; // dereference _ctx
+        Ok(())
+    }
+
+    pub fn withdraw(_ctx: Context<Withdraw>, _amount: u64) -> ProgramResult {
+        let campaign = &mut _ctx.accounts.campaign;
+        let user = &mut _ctx.accounts.user;
+        if *user.key != campaign.admin {
+            return Err(ProgramError::IncorrectProgramId);
+        }
+        // need to put some amount in the account for rent
+        // alternatively, put 2 years rent in account, no rent will be collected - Rent Exemption
+        let rent_balance = Rent::get()?.minimum_balance(campaign.to_account_info().data_len());
+
+        // campaign.to_account_info().lamports.borrow(): Ref<'_, &mut u64>
+        // *campaign.to_account_info().lamports.borrow(): &mut u64
+        if **campaign.to_account_info().lamports.borrow() - rent_balance < _amount {
+            return Err(ProgramError::InsufficientFunds);
+        }
+        **campaign.to_account_info().try_borrow_mut_lamports()? -= _amount;
+        **user.to_account_info().try_borrow_mut_lamports()? += _amount;
+        Ok(())
+    }
+}
+
+#[account]
+#[derive(Default)]
+pub struct Campaign {
+    pub name: String,
+    pub description: String,
+    pub account_donated: u64,
+    pub admin: Pubkey
+}
+
+#[derive(Accounts)]
+pub struct Create<'info> {
+    // init: init calculator account
+    // payer: who pay the cost for creating account
+    // space: the amount of space allocated for the account
+    // seeds, required for storing data
+    #[account(init, payer=user, space=9000,
+    seeds=[b"CAMPAIGN_DEMO".as_ref(), user.key().as_ref()], // solana will use hash function to determine the address of the account
+    bump // add 8-bit bump to the hash function
+    )]
+    pub campaign: Account<'info, Campaign>,
+    pub system_program: Program<'info, System>,
+
+    // required for using 'account', set user become mutable
+    #[account(mut)]
+    pub user: Signer<'info>
+}
+
+#[derive(Accounts)]
+pub struct Withdraw<'info> {
+    #[account(mut)]
+    pub campaign: Account<'info, Campaign>,
+    #[account(mut)]
+    pub user: Signer<'info>
+}
+
